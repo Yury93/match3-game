f5d891e4c35c3bb502f09e2e85e2dab2d4265de6
Yury93 <imezomorf93@gmail.com>
2025-07-28 12:29:10 +0300

    refactor(roadmap): add tweens

--- assets/game/scripts/menu/roadmap/roadmap.ts
+++ assets/game/scripts/menu/roadmap/roadmap.ts
@@ -15,6 +15,9 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export class Roadmap extends cc.Component implements IRoadmap {
   onClickPlay: () => void;
+
+  @property(cc.Sprite)
+  clouds: cc.Sprite = null;
   @property(cc.Label)
   label: cc.Label = null;
   @property(cc.Button)
@@ -31,6 +34,44 @@ export class Roadmap extends cc.Component implements IRoadmap {
       this.clickGameButton,
       this,
     );
+  }
+  protected start(): void {
+    this.startTweenButton();
+    this.startTweenClouds();
+  }
+  private startTweenClouds() {
+    if (!this.clouds) return;
+
+    cc.tween(this.clouds.node)
+      .repeatForever(
+        cc
+          .tween()
+          .to(3.0, { x: this.clouds.node.x + 20 }, { easing: "sineInOut" })
+          .to(3.0, { x: this.clouds.node.x - 20 }, { easing: "sineInOut" }),
+      )
+      .start();
+  }
+  private startTweenButton() {
+    cc.tween(this.gameButton.node)
+      .repeatForever(
+        cc
+          .tween()
+          .to(0.4, {
+            position: cc.v3(
+              this.gameButton.node.x,
+              this.gameButton.node.y + 10,
+            ),
+          })
+          .to(0.4, {
+            position: cc.v3(this.gameButton.node.x, this.gameButton.node.y),
+          })
+          .delay(0.5),
+      )
+      .start();
+
+    cc.tween(this.gameButton.node)
+      .repeatForever(cc.tween().to(0.8, { scale: 1.1 }).to(0.8, { scale: 1.0 }))
+      .start();
   }
   getRoadmapPoints(): RoadmapPoint[] {
     return this.roadmapPoints;
